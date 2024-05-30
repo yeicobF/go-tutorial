19151682b8502b1736e4657788abcbdda52413b2
yeicobF <jacobrdz34@gmail.com>
2024-05-30 12:14:15 -0600

    feat: Add a test
    Note: The code is currently broke, as one argument was removed from the `Hello` function. Fixing it in the next commit.
    https://go.dev/doc/tutorial/add-a-test

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -15,7 +15,8 @@ func Hello(name string) (string, error) {
 
 	// If a name was received, return a value that embeds the name in a greeting
 	// message.
-	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat(), name)
+	message := fmt.Sprintf(randomFormat())
 	return message, nil
 }
 
