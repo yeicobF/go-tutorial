ba4eeadd498c0e73d3880b2a1f1bba4cc0bd6ae1
yeicobF <jacobrdz34@gmail.com>
2024-05-30 12:15:29 -0600

    fix: add missing argument

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -16,7 +16,7 @@ func Hello(name string) (string, error) {
 	// If a name was received, return a value that embeds the name in a greeting
 	// message.
 	// message := fmt.Sprintf(randomFormat(), name)
-	message := fmt.Sprintf(randomFormat())
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
 
